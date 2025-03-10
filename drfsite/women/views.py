64a5e0ea9fd75e92224cf24b1b2180669cbddd3c
IslamDNJ <syleimanov20148@gmail.com>
2025-03-10 17:33:36 +0300

    Add metod put, create, update

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -4,34 +4,36 @@ from rest_framework.response import Response
 from .models import Women
 from .serializers import WomenSerializer
 
+
 # Реализация сериализатора
 class WomenAPIView(APIView):
     def get(self, request):
         # Формирование списка из объекта классов Women
         w = Women.objects.all()
         # Передача параметров на Сериализатор
-        return Response({'posts': WomenSerializer(w, many=True).data}) # many - для списка записей
+        return Response(
+            {"posts": WomenSerializer(w, many=True).data}
+        )  # many - для списка записей
 
     def post(self, request):
         # Проверка на корректность в POST запросе title, content, cat_id
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # В случае отсутствия одного из значений, заданных в serializers.py, ошибка не выдается, а возвращается JSON файл:
-        # {
-        #     "title":[
-        #         "Обязательное поле."
-        #     ],
-        #     "time_create": [
-        #         "Обязательное поле."
-        #     ],
-        #     "time_update": [
-        #         "Обязательное поле."
-        #     ]
-        # }
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],            # request.data используется для получения данных
-            content=request.data['content'],        # которые были отправлены в запросе 
-            cat_id=request.data['cat_id']           # POST, PUT или PATCH
-        )
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({"post": serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
