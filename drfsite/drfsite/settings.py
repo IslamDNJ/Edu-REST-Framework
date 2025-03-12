66aa2ffa86b8b705400cb872562d2cc68a5a347b
IslamDNJ <syleimanov20148@gmail.com>
2025-03-12 23:15:16 +0300

    Add settings for DRF

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Если коментировать строки то меняется вывод на сайте
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
